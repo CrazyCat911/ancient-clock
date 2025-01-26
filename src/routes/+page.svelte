c98e8c82cb4fc7df2b235ec67258b5d25f64517f
CrazyCat911 <127078477+CrazyCat911@users.noreply.github.com>
2025-01-26 21:46:45 +1000

    simple working

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,31 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	let totalslivers = $state(0),
+		totalparts = $derived(Math.floor(totalslivers / 1296)),
+		totalhours = $derived(Math.floor(totalparts / 144)),
+		totaldays = $derived(Math.floor(totalhours / 16));
+
+	onMount(() => {
+		setInterval(() => {
+			totalslivers += 1;
+		}, 3125 / 108);
+	});
+</script>
+
+<div class="clock">
+	days : hours : parts : slivers <br />
+	{totaldays}:{totalhours}:{totalparts}:{totalslivers}
+</div>
+
+<style>
+	@import url('https://fonts.googleapis.com/css2?family=Major+Mono+Display&display=swap');
+
+	.clock {
+		font-family: 'Major Mono Display', monospace;
+		font-size: 3rem;
+		color: #333;
+		text-align: center;
+		margin-top: 10rem;
+	}
+</style>
